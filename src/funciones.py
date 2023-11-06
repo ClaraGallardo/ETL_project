53e991b6a9b5fc5396cbd75f3395a67f2352a6cd
Clara Gallardo <clara.gallardo93@gmail.com>
2023-11-06 02:32:12 +0100

    cambios

--- src/funciones.py
+++ src/funciones.py
@@ -1,6 +1,10 @@
-
+import requests
 
 def limpieza_tipo(df):
+
+    '''
+    Función creada para limpiar toda la columna del dataframe de Tripadvisor
+    '''
 
     df.Tipo = df.Tipo.str.replace(r'(€+)', '', regex=True)
     df.Tipo = df.Tipo.str.replace('Asiática','')
@@ -29,4 +33,50 @@ def limpieza_tipo(df):
     df.Tipo = df.Tipo.str.replace('TurcaDe Oriente Medio','Turca')
     df.Tipo = df.Tipo.str.replace('Sushi','Japonesa')
     df.Tipo = df.Tipo.str.replace('Restaurante de carneBarbacoa','Argentino')
+
+
+def consulta_API(tipo):
+    '''
+    Realiza una consulta a la API de Google Places para buscar restaurantes cercanos de un tipo específico.
+
+    Args:
+        tipo (str): El tipo de restaurante que se desea buscar, por ejemplo, "coreano".
+
+    Returns:
+        list: Una lista de diccionarios con información de restaurantes que coinciden con el tipo especificado.
+    '''
+    key global
+
+    API_URL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
+
+    # Parámetros de la consulta
+    params= {
+    "location": "40.416709,-3.690286",
+    "radius": 20000,
+    "type": "restaurant",
+    "keyword": ["coreano"],
+    "language": "es",
+    "key": key
+    }
+
+    # Realiza la consulta
+    response = requests.get(API_URL, params=params)
+
+    # Comprueba el código de respuesta
+    if response.status_code != 200:
+
+        raise Exception("Error al realizar la consulta: {} {}".format(response.status_code, response.content))
+
+    # Decodifica la respuesta
+    data = json.loads(response.content)
+
+    # Obtiene la lista de resultados
+    results = data["results"]
+
+    # Filtra los resultados por puntuación
+    results = [result for result in results if result["rating"] is not None]
+
+    #Añade el tipo de restaurante
+    for result in results:
+        result["tipo"] = result["types"][0]
     
